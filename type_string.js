9f7af3c6ef2af88a8966812ef8914cba129cb3bc
Allyson Torres <allysonnamikase@gmail.com>
2023-12-06 20:22:39 -0300

    Exemplos da aula 1

---
+++ type_string.js
@@ -0,0 +1,44 @@
+// tipo string
+
+const texte1 = "Olá, mundo!";
+const texte2 = "Olá, mundo!";
+const senha = "senhaSuperSegura456!"
+const StringDeNumeros = "34567";
+
+const citacao = 'Meu nome é ';
+const meuNome = "Allyson"
+
+// concatenação  (+)
+
+console.log(citacao + meuNome)
+
+ // template string ou template literal 
+
+////////////////////////////////////// 
+
+ const cifrao = '\u0024'
+const aMaiusculo = '\u0041'
+const tique = '\u2705'
+const hiragana = '\u3041'
+
+console.log(cifrao)
+console.log(aMaiusculo)
+console.log(tique)
+console.log(hiragana)
+
+
+const cidade = "belo horizonte";
+const input = "Belo Horizonte";
+
+console.log(cidade === input); // false
+
+const cidade1 = "belo horizonte";
+const input2 = "Belo Horizonte";
+
+const inputMinusculo = input2.toLowerCase();
+
+console.log(cidade === inputMinusculo); // true
+
+const senha2 = "minhaSenha123"
+console.log(senha2.length) // 13 caracteres
+
