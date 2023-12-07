f1f664731e3ff04a9e6f45b55a74a8882e184008
Allyson Torres <allysonnamikase@gmail.com>
2023-12-07 12:06:48 -0300

    Codigos da aula 2

---
+++ Aula2/conversoes.js
@@ -0,0 +1,19 @@
+// tipo de dado 
+
+// booleanos
+
+
+// conversão implícita
+const numero = 456;
+const numeroString = Number('456');
+
+
+// NUMBER()
+// STRING()
+console.log(numero == numeroString)
+console.log(numero + numeroString)
+
+// conversão explícita
+
+
+
